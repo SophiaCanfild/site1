a2dd94958dba18ca1e1622239b8f1f31aae62ab3
Paulo190409 <paulo.fidelis@escola.pr.gov.br>
2025-10-18 20:44:43 -0300

    .

--- script.js
+++ script.js
@@ -3,14 +3,3 @@ document.addEventListener("DOMContentLoaded", function () {
   const btnClique = document.getElementById("btn-clique");
   const btnEnviar = document.getElementById("btn-enviar");
   const btnCadastre = document.getElementById("btn-cadastre");
-
-  document.addEventListener("DOMContentLoaded", function () {
-  const cards = document.querySelectorAll(".card");
-
-  cards.forEach(card => {
-    card.addEventListener("click", function () {
-      const inner = card.querySelector(".card-inner");
-      inner.classList.toggle("flipped");
-    });
-  });
-});
