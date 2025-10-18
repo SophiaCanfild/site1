bc3e43470306974c9ef08663a098aac5bc9c5925
Paulo190409 <paulo.fidelis@escola.pr.gov.br>
2025-10-18 18:50:22 -0300

    .

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach(card => {
+    card.addEventListener("click", function () {
+      card.classList.toggle("flipped");
+    });
+  });
+});
+
+
