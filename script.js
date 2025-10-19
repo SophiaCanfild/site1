5404bcd7a0eafa668f8f17a122486d3083743877
ggabibs <maria.barbosa.sales@escola.pr.gov.br>
2025-10-19 19:22:46 -0300

    .

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ document.addEventListener("DOMContentLoaded", function ()
   const btnClique = document.getElementById("btn-clique");
   const btnEnviar = document.getElementById("btn-enviar");
   const btnCadastre = document.getElementById("btn-cadastre");
+
+  public String getContatoSection() {
+    return """
+        <!-- ===== SEÇÃO 4: Contato & redes sociais ===== -->
+        <section class="contato">
+          <div class="contato-inner">
+            <div class="contato-left">
+              <h3 class="redes-title">Siga-nos nas redes sociais</h3>
+              <div class="social-icons">
+                <a href="https://www.instagram.com/colegioszymanski?igsh=MWIwODh5cGh0NTBoaA==" target="_blank">
+                  <img src="insta.png" alt="Instagram">
+                </a>
+                <a href="https://www.facebook.com/share/19kvC9XmbA/" target="_blank">
+                  <img src="face.png" alt="Facebook">
+                </a>
+                <a href="https://api.whatsapp.com/send?phone=%2B554136424089&fbclid=IwVERDUANh3KFleHRuA2FlbQIxMAABHjakSyA-fBkBifkZD_ywjv69ZhaIzHSEyXz0hn0S3yJziTp4RSoIUS5MW1Ua_aem_JjbN2UK18z0WukvSVuUKJA" target="_blank">
+                  <img src="whats.png" alt="WhatsApp">
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+        """;
+}
