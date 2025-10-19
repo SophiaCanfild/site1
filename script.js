41a8594e8c7fdb52fa497c00faec0e08bce1479e
ggabibs <maria.barbosa.sales@escola.pr.gov.br>
2025-10-19 19:57:00 -0300

    .

--- script.js
+++ script.js
@@ -8,26 +8,34 @@ document.addEventListener("DOMContentLoaded", function ()
   const btnEnviar = document.getElementById("btn-enviar");
   const btnCadastre = document.getElementById("btn-cadastre");
 
-  public String getContatoSection() {
-    return """
-        <!-- ===== SEÇÃO 4: Contato & redes sociais ===== -->
-        <section class="contato">
-          <div class="contato-inner">
-            <div class="contato-left">
-              <h3 class="redes-title">Siga-nos nas redes sociais</h3>
-              <div class="social-icons">
-                <a href="https://www.instagram.com/colegioszymanski?igsh=MWIwODh5cGh0NTBoaA==" target="_blank">
-                  <img src="insta.png" alt="Instagram">
-                </a>
-                <a href="https://www.facebook.com/share/19kvC9XmbA/" target="_blank">
-                  <img src="face.png" alt="Facebook">
-                </a>
-                <a href="https://api.whatsapp.com/send?phone=%2B554136424089&fbclid=IwVERDUANh3KFleHRuA2FlbQIxMAABHjakSyA-fBkBifkZD_ywjv69ZhaIzHSEyXz0hn0S3yJziTp4RSoIUS5MW1Ua_aem_JjbN2UK18z0WukvSVuUKJA" target="_blank">
-                  <img src="whats.png" alt="WhatsApp">
-                </a>
+  
               </div>
             </div>
           </div>
         </section>
         """;
+function abrirRedeSocial(rede) {
+  let url = "";
+
+  if (rede === "instagram") {
+    url = "https://www.instagram.com/colegioszymanski?igsh=MWIwODh5cGh0NTBoaA==";
+  } else if (rede === "facebook") {
+    url = "https://www.facebook.com/share/19kvC9XmbA/";
+  } else if (rede === "whatsapp") {
+    url = "https://api.whatsapp.com/send?phone=%2B554136424089&fbclid=IwVERDUANh3KFleHRuA2FlbQIxMAABHjakSyA-fBkBifkZD_ywjv69ZhaIzHSEyXz0hn0S3yJziTp4RSoIUS5MW1Ua_aem_JjbN2UK18z0WukvSVuUKJA";
+  }
+
+  window.open(url, "_blank");
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  const icones = document.querySelectorAll(".social-icons img");
+
+  icones.forEach(img => {
+    const rede = img.alt.toLowerCase();
+    img.addEventListener("click", function(event) {
+      event.preventDefault();
+      abrirRedeSocial(rede);
+    });
+  });
+});
