21bf232fa86c0525bd7536a91c344da0c540e43e
sophia <sophia.saladini@escola.pr.gov.br>
2025-10-19 16:33:07 -0300

    bn

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
- document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
   const images = document.querySelectorAll(".card-image");
 
   images.forEach(image => {
@@ -17,9 +16,9 @@
   });
 });
 
-
-document.addEventListener("DOMContentLoaded", function () 
+document.addEventListener("DOMContentLoaded", function () {
   const btnSaiba = document.getElementById("btn-saiba");
   const btnClique = document.getElementById("btn-clique");
   const btnEnviar = document.getElementById("btn-enviar");
   const btnCadastre = document.getElementById("btn-cadastre");
+});
