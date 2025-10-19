f807f4104024f378850dfd2e2fa6c003efae4b88
Paulo190409 <paulo.fidelis@escola.pr.gov.br>
2025-10-19 17:56:33 -0300

    .

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const images = document.querySelectorAll(".card-image");
+function flipCard(cardElement) {
+  cardElement.classList.toggle('flipped');
+}
 
-  images.forEach(image => {
-    image.addEventListener("click", function () {
-      const card = image.closest(".card");
-
-      // Gira o cartão
-      card.classList.add("flipped");
-
-      // Volta após 10 segundos (10000 milissegundos)
-      setTimeout(() => {
-        card.classList.remove("flipped");
-      }, 10000);
-    });
-  });
-});
 
 
 document.addEventListener("DOMContentLoaded", function () 
