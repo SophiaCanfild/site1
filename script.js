5215744bc12b53a757cdd36ede545723d0a64186
sophia <sophia.saladini@escola.pr.gov.br>
2025-10-19 16:40:23 -0300

    nhh

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-document.addEventListener("DOMContentLoaded", function () {
+
+document.addEventListener("DOMContentLoaded", function () 
   const btnSaiba = document.getElementById("btn-saiba");
   const btnClique = document.getElementById("btn-clique");
   const btnEnviar = document.getElementById("btn-enviar");
   const btnCadastre = document.getElementById("btn-cadastre");
-});
